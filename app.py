10a07780172c329994fa800f8cd37f48339c53e2
Tian Qiu <tian.qiu142@gmail.com>
2020-12-03 16:34:35 -0800

    insert_one to harvest_db

--- app.py
+++ app.py
@@ -90,14 +90,14 @@ def harvest(plant_id):
     # TODO: Create a new harvest object by passing in the form data from the
     # detail page form.
     new_harvest = {
-        'quantity': '',  # e.g. '3 tomatoes'
-        'date': '',
+        'quantity': request.form.get('harvested_amount'),
+        'date': request.form.get('date_planted'),
         'plant_id': plant_id
     }
 
     # TODO: Make an `insert_one` database call to insert the object into the
     # `harvests` collection of the database.
-
+    harvests_db.insert_one(new_harvest)
     return redirect(url_for('detail', plant_id=plant_id))
 
 
